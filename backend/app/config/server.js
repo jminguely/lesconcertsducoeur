d7a70a14a3da410a3a6eb4ce1565851aa5c5c237
gnosed <hiwzster@gmail.com>
2021-06-15 21:55:32 +0200

    Add backend files

---
+++ backend/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cc6d10545f8d63174896ac20273c1901'),
+    },
+  },
+});
---
+++ backend/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ee6c97cc-a86e-423c-b117-a646c0f0a46d'
+};
