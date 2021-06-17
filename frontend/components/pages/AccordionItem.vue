9868e679c5f272c533eaf9c72afa3b243020f4e8
AregGhazaryan <areg.areg51@gmail.com>
2021-06-17 19:10:54 +0400

    concerts page

---
+++ frontend/components/pages/AccordionItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="pt-4 pb-8 border-t border-b border-black">
+    <div class="flex items-center cursor-pointer select-none" @click="open = !open">
+      <svg
+        :class="{ 'transform rotate-90': open }"
+        xmlns="http://www.w3.org/2000/svg"
+        width="28"
+        height="28"
+        fill="currentColor"
+        class="mr-2 duration-300 ease-in-out bi bi-chevron-right"
+        viewBox="0 0 16 16"
+      >
+        <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
+      </svg>
+      <h2 class="text-4xl font-newsCycle"><slot name="label"></slot></h2>
+    </div>
+    <div :class="{ 'duration-300 ease-in-out min-h-0 max-h-0 flex justify-between': true, 'overflow-hidden max-h-0 font-newsCycle': !open, 'max-h-96 overflow-auto pt-4': open }">
+      <div class="w-full px-5">
+        <slot name="content"></slot>
+      </div>
+      <div class="w-full">
+        <slot name="image"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false,
+    }
+  },
+}
+</script>
--- frontend/pages/concerts.vue
+++ frontend/pages/concerts.vue
@@ -25,18 +25,115 @@
       </template>
     </ImageBlock>
 
-    <p class="text-lg font-playFair">Nos concepts, packaging</p>
+    <p class="text-2xl font-playFair">Nos concepts, packaging</p>
+
+    <AccordionItem>
+      <template #label>Le concert traditionnel</template>
+      <template #content>
+        Dans les établissements médico-sociaux et les institutions vouées aux personnes en difficulté nous donnons des concerts sous forme traditionnelle. Nous souhaitons que ces lieux de vie se
+        transforment en salle de concert pour les résidents, le personnel ainsi que les proches le temps d’un après-midi. Afin de garantir une meilleure organisation et des concerts participatifs,
+        nous travaillons en aval avec le personnel des institutions hôtes.
+      </template>
+      <template #image><img class="mx-auto" src="https://placeimg.com/540/360/animals" /></template>
+    </AccordionItem>
+
+    <AccordionItem>
+      <template #label>Les interventions dans les chambres</template>
+      <template #content>
+        Dans les établissements médico-sociaux et les institutions vouées aux personnes en difficulté nous donnons des concerts sous forme traditionnelle. Nous souhaitons que ces lieux de vie se
+        transforment en salle de concert pour les résidents, le personnel ainsi que les proches le temps d’un après-midi. Afin de garantir une meilleure organisation et des concerts participatifs,
+        nous travaillons en aval avec le personnel des institutions hôtes.
+      </template>
+      <template #image><img class="mx-auto" src="https://placeimg.com/540/360/animals" /></template>
+    </AccordionItem>
+
+    <AccordionItem>
+      <template #label>concerts virtuels</template>
+      <template #content>
+        Dans les établissements médico-sociaux et les institutions vouées aux personnes en difficulté nous donnons des concerts sous forme traditionnelle. Nous souhaitons que ces lieux de vie se
+        transforment en salle de concert pour les résidents, le personnel ainsi que les proches le temps d’un après-midi. Afin de garantir une meilleure organisation et des concerts participatifs,
+        nous travaillons en aval avec le personnel des institutions hôtes.
+      </template>
+      <template #image><img class="mx-auto" src="https://placeimg.com/540/360/animals" /></template>
+    </AccordionItem>
+
+    <AccordionItem>
+      <template #label>concerts en terrasse / balcons /extérieur</template>
+      <template #content>
+        Dans les établissements médico-sociaux et les institutions vouées aux personnes en difficulté nous donnons des concerts sous forme traditionnelle. Nous souhaitons que ces lieux de vie se
+        transforment en salle de concert pour les résidents, le personnel ainsi que les proches le temps d’un après-midi. Afin de garantir une meilleure organisation et des concerts participatifs,
+        nous travaillons en aval avec le personnel des institutions hôtes.
+      </template>
+      <template #image><img class="mx-auto" src="https://placeimg.com/540/360/animals" /></template>
+    </AccordionItem>
+
+    <AccordionItem>
+      <template #label>rencontre virtuelle avec les artistes</template>
+      <template #content>
+        Dans les établissements médico-sociaux et les institutions vouées aux personnes en difficulté nous donnons des concerts sous forme traditionnelle. Nous souhaitons que ces lieux de vie se
+        transforment en salle de concert pour les résidents, le personnel ainsi que les proches le temps d’un après-midi. Afin de garantir une meilleure organisation et des concerts participatifs,
+        nous travaillons en aval avec le personnel des institutions hôtes.
+      </template>
+      <template #image><img class="mx-auto" src="https://placeimg.com/540/360/animals" /></template>
+    </AccordionItem>
+
+    <AccordionItem>
+      <template #label>atelier découverte d’instruments</template>
+      <template #content>
+        Dans les établissements médico-sociaux et les institutions vouées aux personnes en difficulté nous donnons des concerts sous forme traditionnelle. Nous souhaitons que ces lieux de vie se
+        transforment en salle de concert pour les résidents, le personnel ainsi que les proches le temps d’un après-midi. Afin de garantir une meilleure organisation et des concerts participatifs,
+        nous travaillons en aval avec le personnel des institutions hôtes.
+      </template>
+      <template #image><img class="mx-auto" src="https://placeimg.com/540/360/animals" /></template>
+    </AccordionItem>
+
+    <p class="my-8 text-2xl font-playFair">Dans l’élaboration du répertoire nous répondons aux directives suivantes:</p>
+
+    <div class="text-xl font-newsCycle">
+      <ul class="mb-5">
+        <li>— faire découvrir un nouveau répertoire afin d’éveiller la curiosité des auditeurs;</li>
+        <li>— interpréter des œuvres spécifiques demandées en amont par le public;</li>
+        <li>— faire revivre le répertoire musical régional;</li>
+        <li>— faire participer le public dans l’interprétation d’un répertoire connu comme des chants régionaux traditionnels.</li>
+      </ul>
+
+      <p class="mb-5">Le concert dure entre 45 minutes et une heure.</p>
+
+      <p class="mb-5">
+        Il est accompagné de médiation. Les artistes se présentent et parlent des pièces qu’ils vont interpréter. La configuration de la salle est primordiale. Idéalement le lieu où se tient le
+        concert se transforme en véritable salle de concert, adaptée pour l’écoute. Généralement, plus le public est groupé, plus il est facile pour les artistes de communiquer.
+      </p>
+
+      <p class="mb-5">Une fois le concert terminé, les musiciens restent sur place pour échanger avec le public.</p>
+    </div>
+
+    <div class="flex justify-between my-56">
+      <div class="max-w-xl">
+        <img src="~/assets/img/illustrations/illustration6.svg" />
+      </div>
+
+      <div class="max-w-xl">
+        <h4 class="flex text-4xl font-newsCycle">
+          <span class="text-6xl">«</span>Votre concert a permis au personnel soignant de constater l’effet bénéfique de la musique sur l’ensemble des résidents. Votre performance a apporté du calme,
+          de la sérénité, beaucoup de joie et de plaisir.<span class="mt-auto text-6xl">»</span>
+        </h4>
+        <h2 class="mt-5 text-4xl text-right font-playFair">Claude Crettol</h2>
+        <h5 class="text-2xl text-right font-newsCycle">directeur des EMS Beaulieu et Jasmins</h5>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Headline from '@/components/typography/Headline.vue'
 import ImageBlock from '@/components/typography/ImageBlock.vue'
+import AccordionItem from '@/components/pages/AccordionItem.vue'
 
 export default {
   components: {
     Headline,
     ImageBlock,
+    AccordionItem,
   },
 }
 </script>
