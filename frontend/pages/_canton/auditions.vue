f58aa317f6601dc72e8e0f6291f08ce0d78872f2
gnosed <hiwzster@gmail.com>
2021-07-08 16:38:20 +0200

    Changing canton switch to canton relation!

--- frontend/pages/_canton/auditions.vue
+++ frontend/pages/_canton/auditions.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="content != null">
-    {{ content.auditions }}
+    {{ content }}
     <Headline>
       <template #headline>
         {{ content.auditions[0].hero.headline }}
@@ -16,29 +16,7 @@
       </template>
     </div>
 
-    <p class="text-2xl font-newsCycle">
-      Avez-vous envie d’apporter des moments de légèreté aux personnes âgées, hospitalisées et/ou en difficulté en leur partageant votre musicalité avec celles-ci tout en développant vos aptitudes de
-      médiation culturelle grâce à des concerts rémunérés ? Si cela vous intéresse, les Associations des Concerts du Coeur Valaisans etVaudois organisent des auditions à Sion et à Lausanne afin de
-      recruter de nouveaux musicien.ne.s.
-    </p>
-
-    <Headline>
-      <template #content>Les artistes intéressés doivent répondre aux critères ci-dessous : </template>
-    </Headline>
-
-    <div class="text-xl font-newsCycle">
-      <p>
-        — la majorité des musiciens du groupe doit résider dans le canton de Vaud ou du Valais; <br />
-        — démontrer un standard très élevé de qualité musicale et technique instrumentale; <br />
-        — posséder de bonnes capacités de communication et une aisance manifeste à établir une relation avec le public en <br />
-        — l’invitant à participer autant que faire se peut; <br />
-        — proposer un choix de répertoire varié et adapté aux différents publics des Concerts du Coeur; <br />
-        — s’engager à respecter et à défendre les valeurs de l’Association.
-      </p>
-      <p class="my-5">Tous les genres de musique sont bienvenus (classique / jazz / folklorique / musique du monde etc.).</p>
-    </div>
-
-    <div class="flex flex-col justify-start my-20 lg:flex-row">
+    <!-- <div class="flex flex-col justify-start my-20 lg:flex-row">
       <ExtendedBlock class="mb-10 lg:mr-28 lg:mb-0">
         <template #datetime> Audition dimanche 11 avril 2021 </template>
         <template #content>
@@ -85,22 +63,23 @@
           </div>
         </template>
       </ExtendedBlock>
-    </div>
+    </div> -->
+    <Spacing />
 
-    <p class="max-w-md text-3xl text-yellow-500 font-playFair lg:mb-36">Nous nous réjouissons de vous rencontrer et de vous écouter !</p>
+    <p class="text-3xl text-yellow-500 font-playFair">Nous nous réjouissons de vous rencontrer et de vous écouter !</p>
   </div>
 </template>
 
 <script>
 import Headline from '@/components/typography/Headline.vue'
-import ExtendedBlock from '@/components/typography/ExtendedBlock.vue'
+import Spacing from '@/components/typography/Spacing.vue'
 
 import { gql } from 'graphql-tag'
 
 export default {
   components: {
     Headline,
-    ExtendedBlock,
+    Spacing,
   },
 
   data() {
