6a9e63a0316bf035a0221a728c6560c024a3c6c9
gnosed <hiwzster@gmail.com>
2021-08-04 20:04:12 +0200

    Concerts page french

--- frontend/i18n/fr.js
+++ frontend/i18n/fr.js
@@ -84,6 +84,97 @@ export default {
       ],
     },
   },
+  concerts: {
+    hero: {
+      title: 'Nos Concerts',
+      subtitle: `Adaptant constamment ses concerts à la situation sanitaire, aux publics et aux lieux, <i>Les Concerts du Cœur</i> cherchent avec vous la meilleure solution pour votre institution.<br>
+        Durant la pandémie, nous n’avons pas annulé les concerts mais avons constamment cherché des alternatives afin de ne jamais interrompre la musique.`,
+      text: `<p class="pb-4">Nous proposons une large palette de programmes musicaux de qualité.<br>
+      Tous nos artistes sont des musicien·ne·s professionnel·le·s formé·e·s pour réagir aux situations inattendues et se mettre au service du public.</p>
+<p class="pb-4">Les concerts peuvent avoir lieu en semaine ou le weekend et à n’importe quelle heure de la journée.</p>
+<p>N’hésitez pas à nous contacter pour tout renseignement. </p>
+`,
+    },
+    concertTypes: [
+      {
+        title: 'Concerts médiation traditionnels',
+        content: `Ces concerts sont organisés sous forme traditionnelle : les lieux de vie se transforment en salle de concert pour les résident·e·s, les malades, les bénéficiaires, le personnel ainsi que les proches : 
+      <ul class="pt-2">
+        <li>— Le concert dure entre 45 minutes et une heure ;</li>
+        <li>— Les artistes se présentent et parlent des pièces qu’ils et elles vont interpréter ;</li>
+        <li>— Une fois le concert terminé, les musicien·ne·s restent sur place pour échanger avec le public.</li>
+      </ul>`,
+      },
+      {
+        title: 'Concerts à l’extérieur',
+        content: `En fonction de la météo et de la situation sanitaire, des concerts à l’extérieur sont également possibles, dans des jardins, sur des terrasses, sur des balcons ou même sur des parkings : 
+      <ul class="pt-2">
+        <li>— Le concert dure entre 45 minutes et une heure ;</li>
+        <li>— Le public peut écouter depuis les balcons ou à distance ;</li>
+        <li>— Les artistes se présentent et parlent des pièces qu’ils et elles vont interpréter ;</li>
+        <li>— Une fois le concert terminé, les musicien·ne·s restent sur place pour échanger avec le public ;</li>
+        <li>— Ces concerts sont souvent sonorisés avec des amplificateurs fournis par l’association.</li>
+      </ul>`,
+      },
+      {
+        title: 'Concerts itinérants',
+        content: `Dans les hôpitaux, les cliniques ou les EMS les artistes peuvent se déplacer avec leurs instruments de service en service  et d’étage en étage : 
+      <ul class="pt-2">
+        <li>— Chaque intervention dure une quinzaine de minutes maximum ;</li>
+        <li>— La musique est destinée aux patient·e·s, à leur proche ainsi qu’au personnel ;</li>
+        <li>— Celles et ceux qui le souhaitent ont l’opportunité de suivre les musicien·e·s à travers l’institution ;</li>
+        <li>— Les musicien·ne·s adaptent leur présentation et leur programme au lieu et au public qu’ils et elles rencontrent.</li>
+      </ul>`,
+      },
+      {
+        title: 'Concerts dans les chambres',
+        content: `La possibilité est offerte aux patient·e·s et aux résident·e·s de recevoir des « mini-concerts » privés : 
+      <ul class="pt-2">
+        <li>— Le concert dure une quinzaine de minutes ;</li>
+        <li>— L’artiste s’installe dans la chambre du/de la patient·e qui peut choisir les pièces qu’il ou elle souhaite entendre parmi le répertoire de l’artiste ;</li>
+        <li>— L’artiste dialogue en tête-à-tête avec son public.</li>
+      </ul>`,
+      },
+      {
+        title: 'Concerts virtuels',
+        content: `Des concerts en live streaming peuvent être diffusés dans les institutions :  de leur domicile ou d’un studio de musique,  les artistes proposent un <i>concert live</i> et sont prêts à ouvrir le dialogue sur la musique qu’ils et elles proposent.<br>
+          Si l’installation technologique de l’institution le permet, il est possible de retransmettre le concert dans tout l’établissement. 
+`,
+      },
+      {
+        title: 'Rencontres virtuelles avec des artistes',
+        content: `Ces rencontres se font au moyen d’une tablette de type iPad fourni par l’institution qui circule de chambre en chambre, assurant la diffusion d’un·e musicien·ne connecté·e ; 
+      <ul class="pt-2">
+        <li>— Cela permet aux résident·e·s d'échanger individuellement avec l'artiste, de lui poser des questions et de choisir des pièces parmi le répertoire de l’artiste ;</li>
+        <li>— Ces rendez-vous musicaux constituent une solution idéale pour les institutions où les résident·e·s ne peuvent pas quitter leur chambre.</li>
+      </ul>`,
+      },
+      {
+        title: 'Répertoire',
+        content: `L'élaboration des programmes et répertoires proposés répond aux critères suivants : 
+      <ul class="pt-2">
+        <li>— Interpréter des œuvres spécifiques demandées en amont par le public ;</li>
+        <li>— Faire revivre le répertoire musical régional ;</li>
+        <li>— Faire participer le public dans l’interprétation d’un répertoire connu comme des chants régionaux traditionnels ;</li>
+        <li>— Faire découvrir un répertoire inédit et accessible, qui éveille la curiosité des auditeurs·trices.</li>
+      </ul>`,
+      },
+      {
+        title: 'Organisation',
+        content: `Pour chaque concert, <i>Les Concerts du Cœur</i> offrent les services suivants :  
+      <ul class="pt-2">
+        <li>— Organisation du concert selon la date et l’horaire souhaités par l’institution ;</li>
+        <li>— Coordination avec les artistes ; </li>
+        <li>— Mise à disposition de flyer et d’affiches prêts à imprimer pour chaque concert ;</li>
+        <li>— Mise à disposition gracieusement de matériel et d’instruments tel un piano électrique ;</li>
+        <li>— Établissement de contrats et de fiches de salaires pour les musicien·ne·s et prise en charge des cotisations sociales ;</li>
+        <li>— Recherche de fonds afin de permettre de soutenir les coûts du concert et d’offrir une rémunération correcte aux musicien·ne·s ;</li>
+        <li>— Accompagnement des musicien·ne·s sur place par un membre de l’association ou un/une bénévole ;</li>
+        <li>— Compensation financière pour les musicien·ne·s en cas d’annulation du fait de la situation sanitaire.</li>
+      </ul>`,
+      },
+    ],
+  },
   canton: {
     VS: 'Valais',
     VD: 'Vaud',
--- frontend/pages/concerts.vue
+++ frontend/pages/concerts.vue
@@ -2,20 +2,12 @@
   <div>
     <Headline>
       <template #headline>Nos concerts</template>
-      <template #content
-        ><p>
-          Nous adaptons constamment nos concerts à la situation sanitaire, aux publics et aux lieux. Nous pouvons chercher avec vous la meilleure solution pour votre institution. N’hésitez pas à nous
-          contacter pour en discuter
-        </p></template
-      >
+      <template #content><p v-html="$t('concerts').hero.subtitle"></p></template>
     </Headline>
-    <p class="mb-16 text-xl font-newsCycle">
-      Dans les établissements médico-sociaux et les institutions vouées aux personnes en difficulté nous donnons des concerts sous forme traditionnelle. Nous souhaitons que ces lieux de vie se
-      transforment en salle de concert pour les résidents, le personnel ainsi que les proches le temps d’un après-midi. Afin de garantir une meilleure organisation et des concerts participatifs, nous
-      travaillons en aval avec le personnel des institutions hôtes.
-    </p>
 
-    <ImageBlock>
+    <p class="mb-12 text-2xl font-newsCycle" v-html="$t('concerts').hero.text"></p>
+
+    <!-- <ImageBlock>
       <template #title>Vous souhaitez organiser un concert?</template>
       <template #content>
         <ul class="my-10 font-newsCycle">
@@ -29,94 +21,18 @@
       <template #image>
         <img class="mx-auto" src="https://placeimg.com/540/360/animals" />
       </template>
-    </ImageBlock>
+    </ImageBlock> -->
+
+    <accordion-item v-for="(item, i) in $t('concerts').concertTypes" :key="i">
+      <template #label>{{ item.title }}</template>
+      <template #content><div v-html="item.content"></div></template>
+      <template #image><img class="mx-auto" :src="images[i]" /></template>
+    </accordion-item>
 
     <Spacing />
 
-    <AccordionItem>
-      <template #label>Le concert traditionnel</template>
-      <template #content>
-        Dans les établissements médico-sociaux et les institutions vouées aux personnes en difficulté nous donnons des concerts sous forme traditionnelle. Nous souhaitons que ces lieux de vie se
-        transforment en salle de concert pour les résidents, le personnel ainsi que les proches le temps d’un après-midi. Afin de garantir une meilleure organisation et des concerts participatifs,
-        nous travaillons en aval avec le personnel des institutions hôtes.
-      </template>
-      <template #image><img class="mx-auto" src="https://placeimg.com/540/360/animals" /></template>
-    </AccordionItem>
-
-    <AccordionItem>
-      <template #label>Les interventions dans les chambres</template>
-      <template #content>
-        Dans les établissements médico-sociaux et les institutions vouées aux personnes en difficulté nous donnons des concerts sous forme traditionnelle. Nous souhaitons que ces lieux de vie se
-        transforment en salle de concert pour les résidents, le personnel ainsi que les proches le temps d’un après-midi. Afin de garantir une meilleure organisation et des concerts participatifs,
-        nous travaillons en aval avec le personnel des institutions hôtes.
-      </template>
-      <template #image><img class="mx-auto" src="https://placeimg.com/540/360/animals" /></template>
-    </AccordionItem>
-
-    <AccordionItem>
-      <template #label>concerts virtuels</template>
-      <template #content>
-        Dans les établissements médico-sociaux et les institutions vouées aux personnes en difficulté nous donnons des concerts sous forme traditionnelle. Nous souhaitons que ces lieux de vie se
-        transforment en salle de concert pour les résidents, le personnel ainsi que les proches le temps d’un après-midi. Afin de garantir une meilleure organisation et des concerts participatifs,
-        nous travaillons en aval avec le personnel des institutions hôtes.
-      </template>
-      <template #image><img class="mx-auto" src="https://placeimg.com/540/360/animals" /></template>
-    </AccordionItem>
-
-    <AccordionItem>
-      <template #label>concerts en terrasse / balcons /extérieur</template>
-      <template #content>
-        Dans les établissements médico-sociaux et les institutions vouées aux personnes en difficulté nous donnons des concerts sous forme traditionnelle. Nous souhaitons que ces lieux de vie se
-        transforment en salle de concert pour les résidents, le personnel ainsi que les proches le temps d’un après-midi. Afin de garantir une meilleure organisation et des concerts participatifs,
-        nous travaillons en aval avec le personnel des institutions hôtes.
-      </template>
-      <template #image><img class="mx-auto" src="https://placeimg.com/540/360/animals" /></template>
-    </AccordionItem>
-
-    <AccordionItem>
-      <template #label>rencontre virtuelle avec les artistes</template>
-      <template #content>
-        Dans les établissements médico-sociaux et les institutions vouées aux personnes en difficulté nous donnons des concerts sous forme traditionnelle. Nous souhaitons que ces lieux de vie se
-        transforment en salle de concert pour les résidents, le personnel ainsi que les proches le temps d’un après-midi. Afin de garantir une meilleure organisation et des concerts participatifs,
-        nous travaillons en aval avec le personnel des institutions hôtes.
-      </template>
-      <template #image><img class="mx-auto" src="https://placeimg.com/540/360/animals" /></template>
-    </AccordionItem>
-
-    <AccordionItem>
-      <template #label>atelier découverte d’instruments</template>
-      <template #content>
-        Dans les établissements médico-sociaux et les institutions vouées aux personnes en difficulté nous donnons des concerts sous forme traditionnelle. Nous souhaitons que ces lieux de vie se
-        transforment en salle de concert pour les résidents, le personnel ainsi que les proches le temps d’un après-midi. Afin de garantir une meilleure organisation et des concerts participatifs,
-        nous travaillons en aval avec le personnel des institutions hôtes.
-      </template>
-      <template #image><img class="mx-auto" src="https://placeimg.com/540/360/animals" /></template>
-    </AccordionItem>
-
-    <p class="my-8 text-2xl font-playFair">Dans l’élaboration du répertoire nous répondons aux directives suivantes:</p>
-
-    <div class="text-xl font-newsCycle">
-      <ul class="mb-5">
-        <li>— faire découvrir un nouveau répertoire afin d’éveiller la curiosité des auditeurs;</li>
-        <li>— interpréter des œuvres spécifiques demandées en amont par le public;</li>
-        <li>— faire revivre le répertoire musical régional;</li>
-        <li>— faire participer le public dans l’interprétation d’un répertoire connu comme des chants régionaux traditionnels.</li>
-      </ul>
-
-      <p class="mb-5">Le concert dure entre 45 minutes et une heure.</p>
-
-      <p class="mb-5">
-        Il est accompagné de médiation. Les artistes se présentent et parlent des pièces qu’ils vont interpréter. La configuration de la salle est primordiale. Idéalement le lieu où se tient le
-        concert se transforme en véritable salle de concert, adaptée pour l’écoute. Généralement, plus le public est groupé, plus il est facile pour les artistes de communiquer.
-      </p>
-
-      <p class="mb-5">Une fois le concert terminé, les musiciens restent sur place pour échanger avec le public.</p>
-    </div>
-
-    <Spacing />
-
-    <div class="flex flex-col justify-between lg:flex-row">
-      <div class="max-w-xl my-5 lg:my-0 lg:mr-20">
+    <div class="grid grid-cols-1 gap-5 md:grid-cols-2">
+      <div class="max-w-xl">
         <img src="~/assets/img/illustrations/illustration6.svg" />
       </div>
 
@@ -136,7 +52,6 @@
 
 <script>
 import Headline from '@/components/typography/Headline.vue'
-import ImageBlock from '@/components/typography/ImageBlock.vue'
 import Spacing from '@/components/typography/Spacing.vue'
 import Testimonial from '@/components/typography/Testimonial.vue'
 import AccordionItem from '@/components/pages/AccordionItem.vue'
@@ -144,10 +59,14 @@ import AccordionItem from '@/components/pages/AccordionItem.vue'
 export default {
   components: {
     Headline,
-    ImageBlock,
     AccordionItem,
     Spacing,
     Testimonial,
+  },
+  data() {
+    return {
+      images: ['/img/mission_1.jpeg', '/img/mission_2.jpeg', '/img/mission_3.jpeg', '/img/mission_4.jpeg', '/img/mission_1.jpeg', '/img/mission_2.jpeg', '/img/mission_3.jpeg', '/img/mission_4.jpeg'],
+    }
   },
 }
 </script>
