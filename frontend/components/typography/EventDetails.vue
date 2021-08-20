02987d2d29eaab4014cf26b4fdd093f03cbb0f96
gnosed <hiwzster@gmail.com>
2021-08-20 16:31:58 +0200

    Text fix agenda

--- frontend/components/typography/EventDetails.vue
+++ frontend/components/typography/EventDetails.vue
@@ -1,21 +1,20 @@
 <template>
-  <div class="flex flex-col justify-between border-b border-gray-800 lg:grid lg:grid-cols-2">
+  <div class="flex flex-col justify-between border-b border-gray-800 lg:grid lg:grid-cols-2 lg:gap-x-10">
     <div class="flex flex-col lg:max-w-xl">
-      <div class="font-playFair text-xl lg:text-3xl" :class="{ 'text-vs': canton == 'vs', 'text-vd': canton == 'vd', 'text-ge': canton == 'ge' }">
+      <div class="mb-2 font-playFair text-2xl lg:text-3xl" :class="{ 'text-vs': canton == 'vs', 'text-vd': canton == 'vd', 'text-ge': canton == 'ge' }">
         <div><slot name="date"></slot></div>
         <div><slot name="location"></slot></div>
       </div>
-      <div class="mb-6 text-4xl lg:text-5xl font-newsCycle lg:mb-0 text-concert-dark">
+      <div class="text-4xl lg:text-5xl font-newsCycle lg:mb-0 text-concert-dark">
         <slot name="title"></slot>
       </div>
     </div>
 
-    <div class="flex flex-col prose lg:pl-10">
-      <div class="w-full h-full"></div>
-      <div class="pb-4 font-playFair lg:text-xl text-xl" :class="{ 'text-vs': canton == 'vs', 'text-vd': canton == 'vd', 'text-ge': canton == 'ge' }">
+    <div class="flex flex-col justify-end">
+      <div class="font-playFair text-xl lg:text-2xl" :class="{ 'text-vs': canton == 'vs', 'text-vd': canton == 'vd', 'text-ge': canton == 'ge' }">
         <slot name="artists"></slot>
       </div>
-      <div class="font-playFair lg:text-xl text-xl" :class="{ 'text-vs': canton == 'vs', 'text-vd': canton == 'vd', 'text-ge': canton == 'ge' }">
+      <div class="font-playFair text-xl lg:text-2xl" :class="{ 'text-vs': canton == 'vs', 'text-vd': canton == 'vd', 'text-ge': canton == 'ge' }">
         <slot name="details"> </slot>
       </div>
     </div>
