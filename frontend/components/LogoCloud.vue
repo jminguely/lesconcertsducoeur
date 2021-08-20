2feb06d7b22511c853b25554fc54a4657b99bc48
gnosed <hiwzster@gmail.com>
2021-08-20 14:34:21 +0200

    remove gap from logo cloud

--- frontend/components/LogoCloud.vue
+++ frontend/components/LogoCloud.vue
@@ -3,7 +3,7 @@
     <div class="mb-10 text-3xl font-playFair">
       <slot name="title"></slot>
     </div>
-    <div :class="{ 'grid grid-cols-2 md:grid-cols-3 md:gap-5': !isHome, 'grid grid-cols-4 2xl:grid-cols-6 md:gap-5': isHome }">
+    <div :class="{ 'grid grid-cols-2 md:grid-cols-3 md:gap-0': !isHome, 'grid grid-cols-4 2xl:grid-cols-6': isHome }">
       <div v-for="(item, i) in logos" :key="i">
         <img class="filter grayscale w-full h-full object-contain" :src="'https://api.lesconcertsducoeur.ch' + item.url" />
       </div>
