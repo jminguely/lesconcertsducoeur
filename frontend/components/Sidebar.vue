f67f59080e620fc01bff3c13ad544b9d989085ac
gnosed <hiwzster@gmail.com>
2021-08-21 10:44:47 +0200

    Changer burger icon, effect scrolling sticky header

--- frontend/components/Sidebar.vue
+++ frontend/components/Sidebar.vue
@@ -1,8 +1,16 @@
 <template>
   <div
     id="sidebar"
-    :class="{ 'h-full': menu }"
-    class="fixed top-0 left-0 z-50 flex flex-col justify-between w-full max-h-screen p-4 py-2 duration-300 ease-in-out bg-white lg:pb-4 lg:px-12 lg:py-4 lg:h-screen lg:max-w-xs lg:w-96"
+    class="px-5 py-3 fixed top-0 left-0 z-50 flex flex-col justify-between w-full max-h-screen duration-300 ease-in-out bg-white lg:pb-4 lg:px-12 lg:py-4 lg:h-screen lg:max-w-xs lg:w-96"
+    :class="{
+      'h-full': menu,
+      'border-0': !stickyHeader,
+      'border-b': stickyHeader,
+      'border-concert-dark': canton == '',
+      'border-vs': canton == 'vs',
+      'border-vd': canton == 'vd',
+      'border-ge': canton == 'ge',
+    }"
   >
     <div :class="{ 'absolute top-5 left-5 text-xl z-10': menu, hidden: !menu }" class="ml-3 lg:block lg:text-xl">
       <nuxt-link :to="switchLocalePath('fr')">fr</nuxt-link> | <nuxt-link :to="switchLocalePath('de')">de</nuxt-link>
@@ -11,16 +19,27 @@
     <div class="lg:absolute lg:top-36">
       <div class="flex justify-between items-center">
         <nuxt-link :to="localePath('/')">
-          <Logo :class="{ 'opacity-0': menu, 'opacity-100': !menu }" class="h-12 lg:h-16" />
+          <Logo :class="{ 'opacity-0': menu, 'opacity-100': !menu, 'h-9': stickyHeader, 'h-12': !stickyHeader }" class="lg:h-16" />
         </nuxt-link>
         <button
           :class="{ opened: menu }"
           :aria-expanded="menu"
-          class="flex w-16 h-16 p-0 bg-transparent border-none cursor-pointer lg:hidden focus:outline-none"
+          class="flex items-center p-0 bg-transparent border-none cursor-pointer lg:hidden focus:outline-none"
           aria-label="Main Menu"
           @click="toggleMenu()"
         >
-          <svg class="w-16 h-16" viewBox="0 0 100 100">
+          <svg :class="{ hidden: menu, block: !menu }" width="56" height="28" viewBox="0 0 56 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <line y1="1" x2="56" y2="1" stroke="#393939" stroke-width="2" />
+            <line y1="14" x2="56" y2="14" stroke="#393939" stroke-width="2" />
+            <line y1="27" x2="56" y2="27" stroke="#393939" stroke-width="2" />
+          </svg>
+
+          <svg :class="{ hidden: !menu, block: menu }" width="56" height="28" viewBox="0 0 56 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <line x1="0.42738" y1="1.09593" x2="55.4274" y2="27.0959" stroke="#393939" stroke-width="2" />
+            <line x1="0.57262" y1="27.0959" x2="55.5726" y2="1.09593" stroke="#393939" stroke-width="2" />
+          </svg>
+
+          <!-- <svg class="w-16 h-16" viewBox="0 0 100 100">
             <path
               class="line line1"
               d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
@@ -30,7 +49,7 @@
               class="line line3"
               d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
             />
-          </svg>
+          </svg> -->
         </button>
       </div>
 
@@ -93,6 +112,7 @@ export default {
   },
   data() {
     return {
+      stickyHeader: false,
       links: [
         { name: this.$t('nav').home, link: this.localePath('/'), exact: true },
         { name: this.$t('nav').mission, link: this.localePath('mission') },
@@ -102,14 +122,37 @@ export default {
       ],
       menu: false,
       cantons: ['vs', 'vd', 'ge'],
+      canton: '',
     }
   },
   watch: {
-    $route(val) {
+    $route(newRoute) {
+      if (newRoute.params) {
+        if (newRoute.params.canton != null) this.canton = newRoute.params.canton
+        else this.canton = ''
+      }
       this.menu = false
     },
   },
+  mounted() {
+    if (this.$route.params) {
+      if (this.$route.params.canton != null) this.canton = this.$route.params.canton
+      else this.canton = ''
+    }
+  },
+  beforeMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
   methods: {
+    handleScroll() {
+      // Your scroll handling here
+      console.log(window.scrollY)
+      if (window.scrollY > 80) this.stickyHeader = true
+      else this.stickyHeader = false
+    },
     openMenu() {
       this.menu = true
     },
@@ -125,35 +168,35 @@ export default {
 <style>
 .line {
   fill: none;
-  stroke: black;
-  stroke-width: 6;
-  transition: stroke-dasharray 600ms cubic-bezier(0.4, 0, 0.2, 1), stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1);
+  stroke: #393939;
+  stroke-width: 2;
+  /* transition: stroke-dasharray 600ms cubic-bezier(0.4, 0, 0.2, 1), stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1); */
 }
 .line1 {
   stroke-dasharray: 60 207;
-  stroke-width: 6;
+  stroke-width: 2;
 }
 .line2 {
   stroke-dasharray: 60 60;
-  stroke-width: 6;
+  stroke-width: 2;
 }
 .line3 {
   stroke-dasharray: 60 207;
-  stroke-width: 6;
+  stroke-width: 2;
 }
 .opened .line1 {
   stroke-dasharray: 90 207;
   stroke-dashoffset: -134;
-  stroke-width: 6;
+  stroke-width: 2;
 }
 .opened .line2 {
   stroke-dasharray: 1 60;
   stroke-dashoffset: -30;
-  stroke-width: 6;
+  stroke-width: 2;
 }
 .opened .line3 {
   stroke-dasharray: 90 207;
   stroke-dashoffset: -134;
-  stroke-width: 6;
+  stroke-width: 2;
 }
 </style>
