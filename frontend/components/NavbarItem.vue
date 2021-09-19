218ae572351a1baba4b49bd57ea7a813bbd07c52
gnosed <hiwzster@gmail.com>
2021-09-19 16:17:09 +0200

    Fix safari bug

--- frontend/components/NavbarItem.vue
+++ frontend/components/NavbarItem.vue
@@ -1,43 +1,45 @@
 <template>
   <li :class="classObj">
-    <div v-clickaway="closeDropdown" class="h-5 flex items-center text-xl lg:text-base" @click="toggleDropdown()">
-      <svg
-        id="Capa_1"
-        :class="{ 'rotate-45': dropdown, 'text-vs': canton == 'vs', 'text-vd': canton == 'vd', 'text-ge': canton == 'ge' }"
-        class="fill-current w-5 h-5 duration-200 ease-in-out transform"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        x="0px"
-        y="0px"
-        viewBox="0 0 512 512"
-        style="enable-background: new 0 0 512 512"
-        xml:space="preserve"
-      >
-        <g>
+    <div v-clickaway="closeDropdown" class="flex items-center text-xl lg:text-base" @click="toggleDropdown()">
+      <div class="h-5 w-5">
+        <svg
+          id="Capa_1"
+          :class="{ 'rotate-45': dropdown, 'text-vs': canton == 'vs', 'text-vd': canton == 'vd', 'text-ge': canton == 'ge' }"
+          class="fill-current w-5 h-5 duration-200 ease-in-out transform"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          viewBox="0 0 512 512"
+          style="enable-background: new 0 0 512 512"
+          xml:space="preserve"
+        >
           <g>
-            <path
-              d="M492,236H276V20c0-11.046-8.954-20-20-20c-11.046,0-20,8.954-20,20v216H20c-11.046,0-20,8.954-20,20s8.954,20,20,20h216
+            <g>
+              <path
+                d="M492,236H276V20c0-11.046-8.954-20-20-20c-11.046,0-20,8.954-20,20v216H20c-11.046,0-20,8.954-20,20s8.954,20,20,20h216
 			v216c0,11.046,8.954,20,20,20s20-8.954,20-20V276h216c11.046,0,20-8.954,20-20C512,244.954,503.046,236,492,236z"
-            />
+              />
+            </g>
           </g>
-        </g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-      </svg>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+        </svg>
+      </div>
       <span class="pl-2 text-xl" :class="{ 'text-vs': canton == 'vs', 'text-vd': canton == 'vd', 'text-ge': canton == 'ge' }">
         <slot name="content"></slot>
       </span>
