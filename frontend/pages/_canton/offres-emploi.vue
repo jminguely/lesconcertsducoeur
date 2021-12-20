f5a312f0786f68386d3039f20740752a5a598285
Julien Minguely <jminguely@gmail.com>
2021-12-20 17:25:17 +0100

    Updates offre demploie vue

--- frontend/pages/_canton/offres-emploi.vue
+++ frontend/pages/_canton/offres-emploi.vue
@@ -1,28 +1,44 @@
 <template>
   <div class="text-xl">
     <Headline>
-      <template #headline>{{ $t('auditions').hero.title }}</template>
-      <template #content> <span v-html="$t('auditions').hero.subtitle[canton]"></span></template>
+      <template #headline>Offres d'emploi</template>
+      <template #content> L’Association <i>Les Concerts du Cœur Vaudois</i> cherche un.en&nbsp;:</template>
     </Headline>
 
+    <h2 class="mt-8 mb-4 text-2xl lg:text-3xl font-playFair">Chargé.e d’administration</h2>
     <p class="text-xl mt-4">
-      {{ $t('auditions').hero.text }}
+      <b>Type de contrat&nbsp;:</b> salarié à un taux de 15 % CDI<br />
+      <b>Entrée en fonction&nbsp;:</b> 1 février 2022 ou à convenir<br />
+      <b>Salaire&nbsp;:</b> CHF 750.- brut par mois <br />
+      <b>Lieu de travail&nbsp;:</b> à domicile avec déplacements occasionnels
     </p>
 
-    <br />
+    <h2 class="mt-4 text-2xl lg:text-3xl">Cahier des charges</h2>
+    <ul class="pt-2">
+      <li>— Tenue de la comptabilité de l’association ;</li>
+      <li>— Gestion des bases de données et des fichiers administratifs ;</li>
+      <li>— Préparation et envoi des contrats aux artistes et aux institutions ;</li>
+      <li>— Facturation des clients ;</li>
+      <li>— Préparation et envoi des fiches et certificats de salaire ;</li>
+      <li>— Gestion des décomptes de charges sociales et de l’impôt à la source ;</li>
+      <li>— Gestion des membres de l'association, classement et archivage des documents, traitement du courrier, prise des PVs.</li>
+    </ul>
 
+    <h2 class="mt-8 mb-2 text-2xl lg:text-3xl">Votre mission</h2>
+    <p class="text-xl">Assister la direction dans l’ensemble des tâches administratives liées à la production des <i>Concerts du Coeur</i> sur le territoire vaudois.</p>
+
+    <h2 class="mt-8 text-2xl lg:text-3xl">Votre profil</h2>
+    <ul class="pt-2">
+      <li>— Vous bénéficiez d’une expérience solide en comptabilité et en administration ;</li>
+      <li>— Vous montrez un intérêt particulier pour les domaines de la culture et du social ;</li>
+      <li>— Vous maîtrisez les programmes de la suite Microsoft Office ;</li>
+      <li>— Vous faites preuve d’un sens prononcé de l’organisation, savez gérer les priorités et disposez d’un esprit d’initiative.</li>
+    </ul>
+
+    <h2 class="mt-8 mb-2 text-2xl lg:text-3xl">Votre dossier de candidature</h2>
     <p class="text-xl">
-      {{ $t('auditions').hero.text_1 }}
+      Merci de nous faire parvenir une lettre de motivation et votre CV par mail à l’adresse : <a href="mailto:vaud@lesconcertsducoeur.ch">vaud@lesconcertsducoeur.ch</a> avant le 7 janvier 2022.
     </p>
-
-    <div v-html="$t('auditions').requirements[canton][0]"></div>
-
-    <br />
-
-    <p>{{ $t('auditions').requirements[canton][1] }}</p>
-    <h2 class="mt-12 mb-4 text-xl lg:text-3xl font-playFair">{{ $t('auditions').prochainesAuditions }}</h2>
-    <p class="text-xl" :class="{ 'text-vs': canton == 'vs', 'text-vd': canton == 'vd', 'text-ge': canton == 'ge' }" v-html="$t('auditions').requirements[canton][2]"></p>
-
     <Spacing />
   </div>
 </template>
