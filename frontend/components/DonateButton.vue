bcdb720c5535b74c39ab85f7289753c4e8996c4a
Julien Minguely <jminguely@gmail.com>
2022-04-08 17:44:52 +0200

    Makes the button smaller for german

--- frontend/components/DonateButton.vue
+++ frontend/components/DonateButton.vue
@@ -1,29 +1,6 @@
 <template>
   <div
-    class="
-      font-playFair
-      duration-500
-      ease-in-out
-      hover:scale-105
-      w-14
-      h-14
-      text-sm
-      sm:w-28 sm:h-28
-      rounded-full
-      text-white
-      flex
-      items-center
-      justify-center
-      text-center
-      transform
-      -rotate-25
-      sm:text-xl
-      absolute
-      -bottom-10
-      m-auto
-      left-0
-      right-0
-    "
+    class="font-playFair duration-500 ease-in-out hover:scale-105 w-14 h-14 text-sm sm:w-28 sm:h-28 rounded-full text-white flex items-center justify-center text-center transform -rotate-25 absolute -bottom-10 m-auto left-0 right-0"
     :class="{ 'bg-vs': canton == 'vs', 'bg-vd': canton == 'vd', 'bg-ge': canton == 'ge', 'bg-concert-dark': canton == '' }"
   >
     <slot></slot>
