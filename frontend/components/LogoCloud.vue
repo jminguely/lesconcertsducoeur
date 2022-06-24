cdf27eb7c22b58b38b5ca10c9298c6b4ee611692
Julien Minguely <jminguely@gmail.com>
2022-06-25 01:29:30 +0200

    Simplify the layout

--- frontend/components/LogoCloud.vue
+++ frontend/components/LogoCloud.vue
@@ -3,7 +3,7 @@
     <div class="mb-10 text-3xl font-playFair">
       <slot name="title"></slot>
     </div>
-    <div :class="{ 'grid grid-cols-2 sm:grid-cols-3 2xl:grid-cols-4': isPartner, 'grid grid-cols-2 sm:grid-cols-3 2xl:grid-cols-4': !isPartner }">
+    <div class="grid grid-cols-2 sm:grid-cols-3 2xl:grid-cols-4">
       <div v-for="(item, i) in logos" :key="i">
         <img class="logo-item filter grayscale w-full h-full object-contain" :src="'https://api.lesconcertsducoeur.ch' + item.url" />
       </div>
