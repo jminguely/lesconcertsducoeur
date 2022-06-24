04dfa45288cd7b7537bc707f32a63dc3afac4f79
Julien Minguely <jminguely@gmail.com>
2022-06-24 10:47:59 +0200

    Logo partner: update layout

--- frontend/components/LogoCloud.vue
+++ frontend/components/LogoCloud.vue
@@ -3,7 +3,7 @@
     <div class="mb-10 text-3xl font-playFair">
       <slot name="title"></slot>
     </div>
-    <div :class="{ 'grid grid-cols-2 sm:grid-cols-4 2xl:grid-cols-6': isPartner, 'grid grid-cols-3 sm:grid-cols-4 2xl:grid-cols-6': !isPartner }">
+    <div :class="{ 'grid grid-cols-2 sm:grid-cols-4 2xl:grid-cols-6': isPartner, 'grid grid-cols-2 sm:grid-cols-3 2xl:grid-cols-4': !isPartner }">
       <div v-for="(item, i) in logos" :key="i">
         <img class="filter grayscale w-full h-full object-contain" :src="'https://api.lesconcertsducoeur.ch' + item.url" />
       </div>
