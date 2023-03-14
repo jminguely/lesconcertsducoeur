be2f5e9ce3e853dc244a104ab1011562fc0d78a1
Julien Minguely <jminguely@gmail.com>
2023-03-14 18:45:54 +0100

    Revert "Update the config folder and prepare strapi 4 reference"
    This reverts commit 71e5af0c164c5557d1184307a4d40c809bb0c615.

---
+++ backend/config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = {
+  //
+  graphql: {
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: false,
+    depthLimit: 7,
+    amountLimit: 150,
+    apolloServer: {
+      tracing: false,
+    },
+  },
+};
