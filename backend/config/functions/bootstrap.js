cd9d6bab93bda2c8585049705875d1c0c1ebb679
Julien Minguely <jminguely@gmail.com>
2026-05-16 16:38:14 +0200

    fix no id undefined on production

--- backend/config/functions/bootstrap.js
+++ backend/config/functions/bootstrap.js
@@ -10,4 +10,21 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = () => {}
+module.exports = () => {
+  const originalWarn = strapi.log.warn.bind(strapi.log)
+
+  strapi.log.warn = (...args) => {
+    const message = args[0]
+
+    // Strapi v3 can emit this warning for internal i18n-generated clauses
+    // even when user queries are valid. Keep logs clean by filtering only this case.
+    if (
+      typeof message === 'string' &&
+      message.includes("The value of field: 'id', in your where filter, is undefined.")
+    ) {
+      return
+    }
+
+    originalWarn(...args)
+  }
+}
